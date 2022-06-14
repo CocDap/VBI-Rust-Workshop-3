c9a8eed6f922b7d26caaf2f3a32d140276904f97
CocDap <hodinhdung1997@gmail.com>
2022-06-14 22:05:09 +0900

    feat: update exercise ownership and borrowing

--- exercise-ownership-borrowing/src/main.rs
+++ exercise-ownership-borrowing/src/main.rs
@@ -1,9 +1,10 @@
 
-fn main(){
+// fn main(){
 
-}
+// }
 
 //Exercise 1
+// Mục đích: giải quyết vấn đề ownership and borrowing không dùng clone()
 // fn main() {
     
 //     let x = change_value(10,20);
@@ -24,7 +25,8 @@ fn main(){
 
 
 //Exercise 2
-
+// Mục đích: giải quyết vấn đề ownership và borrowing ko dùng clone()
+// Các bạn có thể sửa thêm logic để đúng với mục đichs bài này là liệt kê các số nguyên tố 
 // fn main() {
 //     let mut count: u32 = 1;
 //     let mut num: u64 = 1;
@@ -54,6 +56,7 @@ fn main(){
 
 
 //Exercise 3
+// Mục đích: giải quyết vấn đề ownership and borrowing ko dùng clone()
 // fn main() {
 //     let mut values = vec![10, 11, 12];
 //     let v = &mut values;
@@ -75,26 +78,28 @@ fn main(){
 // }
 
 
-// //Exercise 4
-// fn main(){
-//     let  a = vec![1,2,3,4,5];
-//     let  i = 0;
-//     let c = 0;
-//     loop {
-//         let (a, c) = test(a.clone());
-//         println!("{}",c);
-//         if i >=5 {break;}
-//     }
-// }
+//Exercise 4
+// Mục đích : giải quyết vấn đề ownership và borrowing ko dùng clone()
+// Logic hiện tại đang sai (cho 1 vec -> đảo chiều vector đó)
+fn main(){
+    let  a = vec![1,2,3,4,5];
+    let  i = 0;
+    let c = 0;
+    loop {
+        let (a, c) = test(a);
+        println!("{}",c);
+        if i >=5 {break;}
+    }
+}
 
-// pub fn test(mut a: Vec<u8>) -> (Vec<u8>, i32) {
-//     let mut b:Vec<u8>  = Vec::new();
-//     let mut c:u8 = 0;
-//     loop {
-//         if a.len() == 0 { break; }
-//         let d = a.pop().unwrap();
-//         c = c+d;
-//         b.push(d);
-//     }
-//     (b, c as i32)
-// }
+pub fn test(mut a: Vec<u8>) -> (Vec<u8>, i32) {
+    let mut b:Vec<u8>  = Vec::new();
+    let mut c:u8 = 0;
+    loop {
+        if a.len() == 0 { break; }
+        let d = a.pop().unwrap();
+        c = c+d;
+        b.push(d);
+    }
+    (b, c as i32)
+}
